4074f43591c6694b3c7524c05b4ef25db93080b7
Iskandar Sharipov <koklok444@gmail.com>
2024-09-19 01:08:58 +0500

    .

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,3 +1,16 @@
-<div>
-    <!-- Simplicity is the ultimate sophistication. - Leonardo da Vinci -->
-</div>
+<!DOCTYPE html>
+<html lang="en" class="dark scroll-smooth" dir="ltr">
+<head>
+    <meta charset="UTF-8">
+    <title>Hously - Tailwind CSS Real Estate Landing & Admin Dashboard Template</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Tailwind CSS Saas & Software Landing Page Template">
+    <meta name="keywords" content="agency, application, business, clean, creative, cryptocurrency, it solutions, modern, multipurpose, nft marketplace, portfolio, saas, software, tailwind css">
+    <meta name="author" content="Shreethemes">
+    <meta name="website" content="https://shreethemes.in">
+    <meta name="email" content="support@shreethemes.in">
+    <meta name="version" content="2.2.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    @vite('resources/css/app.css')
+</head>
--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,3 +1,62 @@
-<div>
-    <!-- Do what you can, with what you have, where you are. - Theodore Roosevelt -->
-</div>
+<nav id="topnav" class="defaultscroll is-sticky">
+    <div class="container relative">
+        <!-- Start Logo container-->
+        <a class="logo" href="index.html">
+                    <span class="inline-block dark:hidden">
+                        <img src="../assets/images/logo-dark.png" class="l-dark" height="24" alt="">
+                        <img src="../assets/images/logo-light.png" class="l-light" height="24" alt="">
+                    </span>
+            <img src="../assets/images/logo-light.png" height="24" class="hidden dark:inline-block" alt="">
+        </a>
+        <!-- End Logo container-->
+
+        <!-- Start Mobile Toggle -->
+        <div class="menu-extras">
+            <div class="menu-item">
+                <a class="navbar-toggle" id="isToggle" onclick="toggleMenu()">
+                    <div class="lines">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </div>
+                </a>
+            </div>
+        </div>
+        <!-- End Mobile Toggle -->
+
+        <!--Login button Start-->
+        <ul class="buy-button list-none mb-0">
+            <li class="inline mb-0">
+                <a href="#"
+                   class="btn btn-icon bg-green-600 hover:bg-green-700 border-green-600 dark:border-green-600 text-white rounded-full"><i
+                        data-feather="user" class="size-4 stroke-[3]"></i></a>
+            </li>
+            <li class="sm:inline ps-1 mb-0 hidden">
+                <a href="/register"
+                   class="btn bg-green-600 hover:bg-green-700 border-green-600 dark:border-green-600 text-white rounded-full">Signup</a>
+            </li>
+
+            <li class="sm:inline ps-1 mb-0 hidden">
+                <a href="/logout"
+                   class="btn bg-red-600 hover:bg-red-700 border-red-600 dark:border-red-600 text-white hover:text-white rounded-full">
+                    Logout
+                </a>
+            </li>
+        </ul>
+        <!--Login button End-->
+
+        <div id="navigation">
+            <!-- Navigation Menu-->
+            <ul class="navigation-menu justify-end nav-light">
+                <li class="">
+                    <a href="/">E'lonlar</a><span class="menu-arrow"></span>
+                </li>
+                <li><a href="/branches" class="sub-menu-item">Filiallar</a></li>
+
+                <li><a href="/about" class="sub-menu-item">About</a></li>
+
+                <li><a href="contact.html" class="sub-menu-item">Contact</a></li>
+            </ul><!--end navigation menu-->
+        </div><!--end navigation-->
+    </div><!--end container-->
+</nav><!--end header-->
